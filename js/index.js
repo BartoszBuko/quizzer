a3d0a9238a69d1bd943400c4887e52839ee1898e
BartoszBuko <bukobartosz@gmail.com>
2022-03-03 15:24:07 +0100

    fixed reload

--- js/index.js
+++ js/index.js
@@ -1,13 +1,28 @@
 const joinNowBtn = document.querySelector("[data-join-now]");
 const body = document.querySelector("body");
 
+let isVisited = false;
+
 joinNowBtn.addEventListener("click", () => {
   if (window.innerWidth >= 1440) {
     body.style.overflow = "hidden";
     joinNowBtn.classList.add("animate-join-desktop");
     setTimeout(() => {
       window.location.href = "./pages/quizes.html";
+      isVisited = true;
+    }, 500);
+    return;
+  }
+  if (window.innerWidth < 1440) {
+    joinNowBtn.classList.add("animate-join");
+    setTimeout(() => {
+      window.location.href = "./pages/quizes.html";
+      isVisited = true;
     }, 500);
   }
-  joinNowBtn.classList.add("animate-join");
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  if (isVisited) console.log("reload");
+  isVisited = false;
 });
