84f7fd993c6d83477b91431eda9923e98890887e
BartoszBuko <bukobartosz@gmail.com>
2022-03-03 15:31:05 +0100

    js fix

--- js/index.js
+++ js/index.js
@@ -1,25 +1,20 @@
 const joinNowBtn = document.querySelector("[data-join-now]");
 const body = document.querySelector("body");
 
-if (
-  joinNowBtn.classList.contains("animate-join-desktop") ||
-  joinNowBtn.classList.contains("animate-join")
-) {
-  window.location.reload();
-}
 joinNowBtn.addEventListener("click", () => {
   if (window.innerWidth >= 1440) {
     body.style.overflow = "hidden";
     joinNowBtn.classList.add("animate-join-desktop");
     setTimeout(() => {
       window.location.href = "./pages/quizes.html";
+      joinNowBtn.classList.remove("animate-join-desktop");
     }, 500);
   }
   if (window.innerWidth < 1440) {
     joinNowBtn.classList.add("animate-join");
     setTimeout(() => {
       window.location.href = "./pages/quizes.html";
-      isVisited = true;
+      joinNowBtn.classList.remove("animate-join-desktop");
     }, 500);
   }
 });
